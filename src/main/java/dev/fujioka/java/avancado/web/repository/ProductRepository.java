96628c3a1a1ce694833b68b51deb13869a7065f2
mgbento <marcelobento21@gmail.com>
2019-10-03 00:13:38 -0300

    subiu

---
+++ src/main/java/dev/fujioka/java/avancado/web/repository/ProductRepository.java
@@ -0,0 +1,21 @@
+package dev.fujioka.java.avancado.web.repository;
+
+import dev.fujioka.java.avancado.web.domain.Product;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository
+     extends JpaRepository<Product, Long> {
+	
+	@Query("SELECT p FROM Product p WHERE p.id = :id")
+	public Product buscarPorId(Long id);
+
+	@Query("SELECT p FROM Product p WHERE p.name = :name")
+	public Product buscarProdutoPorNome(String name);
+}
